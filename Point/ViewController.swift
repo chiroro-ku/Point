2455583d92b059f402b88e969ea1d72c284f7b1b
chiroro-ku <chiroro1582@gmail.com>
2022-11-18 17:48:26 +0900

    Initial Commit

---
+++ Point/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Point
+//
+//  Created by 小林千浩 on 2022/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
